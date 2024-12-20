ae2ad9eddfee0049968c1f7ae37ad1152ea08250
azimjon-95 <>
2024-12-20 14:39:33 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { useEffect } from 'react';
 
 function App() {
-  useEffect(() => {
-    // Telegram Web App API-ni o‘rnating
-    if (window.Telegram && window.Telegram.WebApp) {
-      // Boshlanishda yarmini ochish
-      window.Telegram.WebApp.setHeaderColor("red"); // Foydalanuvchi interfeysini moslash
-      window.Telegram.WebApp.expand({
-        expandToFullScreen: false, // To‘liq ekran rejimini o‘chirish
-      });
-    } else {
-      console.warn("Telegram WebApp API mavjud emas!");
-    }
-  }, []);
+  // useEffect(() => {
+  //   // Telegram Web App API-ni o‘rnating
+  //   if (window.Telegram && window.Telegram.WebApp) {
+  //     // Boshlanishda yarmini ochish
+  //     window.Telegram.WebApp.setHeaderColor("red"); // Foydalanuvchi interfeysini moslash
+  //     window.Telegram.WebApp.expand({
+  //       expandToFullScreen: false, // To‘liq ekran rejimini o‘chirish
+  //     });
+  //   } else {
+  //     console.warn("Telegram WebApp API mavjud emas!");
+  //   }
+  // }, []);
 
   return (
     <div>
